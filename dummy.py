b18ac8cb0f833b718421cb2b4da595f8e7f2459e
Robin de Boer <robindboer@gmail.com>
2017-02-12 09:13:55 +0100

    post json to server

--- dummy.py
+++ dummy.py
@@ -1,9 +1,9 @@
 import hashlib
+import requests
 import random
 import json
 import string
 import datetime
-import csv
 import os
 
 names_file = "names.txt"
@@ -14,43 +14,48 @@ adresses = open(adresses_file).read().splitlines()
 
 
 def genRandomHash():
-    return hashlib.sha256(str(random.random())).hexdigest()
+    return hashlib.sha256(str(random.random()).encode('utf-8')).hexdigest()
 
-def genHashList():
-    hashList = []
-    for i in range(random.randint(0, 20)):
-        hashList.append(str(genRandomHash()))
-    return hashList
 
 def genRandomName(n):
-    name = names[random.randint(0,len(names)-1)].split()
+    name = names[random.randint(0, len(names)-1)].split()
     return name[n]
 
+
 def genRandomPhone():
-    return"6"+''.join(random.SystemRandom().choice(string.digits) for _ in range(10))
+    return "6" + ''.join(random.SystemRandom().choice(string.digits) for _ in range(10))
+
 
 def genRandomNumberString(n):
     return ''.join(random.SystemRandom().choice(string.digits) for _ in range(9))
+
 
 def genRandomAdress():
     return adresses[random.randint(0,len(adresses)-1)]+" "+str(random.randint(0,300))
 
 
 def gendummyDict():
-    docs = [{"path": "check.pdf",
-            "hash":genRandomHash(),
+    docs = [
+        {
+            "path": "check.pdf",
+            "hash": genRandomHash(),
             "timestamp": str(datetime.datetime.utcnow()),
-            "source":"NestEgg" ,
-            "location": "NestEgg"}]
-    params = {'firstname': genRandomName(0),
-            "lastname":genRandomName(1),
-            "address":genRandomAdress(),
-            "phone":genRandomPhone(),
-            "ssn":genRandomNumberString(9),
-            "acctnum":genRandomNumberString(10),
-            "docs": docs}
-    pensionDict = {"request": "makepension", "params":params}
+            "source": "NestEgg",
+            "location": "NestEgg"
+        }
+    ],
+    params = {
+        'firstname': genRandomName(0),
+        "lastname": genRandomName(1),
+        "address": genRandomAdress(),
+        "phone": genRandomPhone(),
+        "ssn": genRandomNumberString(9),
+        "acctnum": genRandomNumberString(10),
+        "docs": docs
+    }
+    pensionDict = {"request": "makepension", "params": params}
     return pensionDict
+
 
 def genDummyData():
     if not os.path.exists("dummy"):
@@ -58,6 +63,9 @@ def genDummyData():
     for i in range(50):
         f = open('dummy/dummy'+str(i)+'.json', 'w')
         json.dump(gendummyDict(), f)
-    return
 
-genDummyData()
+
+if __name__ == "__main__":
+    # genDummyData()
+    r = requests.post('http://localhost:1337/POST', json=gendummyDict())
+    print(r)
